b493c382b29bdb3658a92ce90e962a8a033bf8fc
ioedeveloper <daviddisu8@gmail.com>
2020-08-18 14:45:28 +0100

    Added new themes and fixed failing flatly theme click in tests

--- apps/remix-ide/test-browser/helpers/init.js
+++ apps/remix-ide/test-browser/helpers/init.js
@@ -32,6 +32,6 @@ function initModules (browser, callback) {
     .clickLaunchIcon('settings')
     .setValue('[data-id="settingsTabGistAccessToken"]', process.env.gist_token)
     .click('[data-id="settingsTabSaveGistToken"]')
-    .click('[data-id="settingsTabThemeFlatly"]') // e2e tests were initially developed with Flatly. Some tests are failing with the default one (Dark), because the dark theme put uppercase everywhere.
+    .click('[data-id="settingsTabThemeLabelFlatly"]') // e2e tests were initially developed with Flatly. Some tests are failing with the default one (Dark), because the dark theme put uppercase everywhere.
     .perform(() => { callback() })
 }
